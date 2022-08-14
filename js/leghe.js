8c003e8aba98eed522804ed6ca666f3cf6d0d80a
Sandro Torri <sandro.torri@ellysse.it>
2022-08-14 09:00:30 +0200

    agg

--- js/leghe.js
+++ js/leghe.js
@@ -7,7 +7,7 @@ matchs['CIS-Q-4'] = {"id":"1360805", "lega":"CIS-Q", "giornata":4, "data":"04/04
 
 //CIS - Finali
 matchs['CIS-F-1'] = {"id":"1405899", "lega":"CIS-F", "giornata":1, "data":"01/08/2022", "daCaricare" : true};
-matchs['CIS-F-2'] = {"id":"", "lega":"CIS-F", "giornata":2, "data":"//2022", "daCaricare" : true};
+matchs['CIS-F-2'] = {"id":"1412297", "lega":"CIS-F", "giornata":2, "data":"22/08/2022", "daCaricare" : true};
 matchs['CIS-F-3'] = {"id":"", "lega":"CIS-F", "giornata":3, "data":"//2022", "daCaricare" : true};
 matchs['CIS-F-4'] = {"id":"", "lega":"CIS-F", "giornata":4, "data":"//2022", "daCaricare" : true};
 matchs['CIS-F-5'] = {"id":"", "lega":"CIS-F", "giornata":5, "data":"//2022", "daCaricare" : true};
