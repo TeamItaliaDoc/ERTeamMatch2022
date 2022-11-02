104a639e8b41a48ecf20dcaee35882dbcf733092
Sandro Torri <sandro.torri@ellysse.it>
2022-11-02 08:26:56 +0100

    agg

--- js/leghe.js
+++ js/leghe.js
@@ -1,18 +1,17 @@
-// 2023
-//1418127
 
 //CIS - Qualificazioni
 matchs['CIS-Q-1'] = {"id":"1326201", "lega":"CIS-Q", "giornata":1, "data":"10/01/2022", "daCaricare" : true};
 matchs['CIS-Q-2'] = {"id":"1333915", "lega":"CIS-Q", "giornata":2, "data":"31/01/2022", "daCaricare" : true};
 matchs['CIS-Q-3'] = {"id":"1344383", "lega":"CIS-Q", "giornata":3, "data":"21/02/2022", "daCaricare" : true};
-//??????matchs['CIS-Q-4'] = {"id":"1360805", "lega":"CIS-Q", "giornata":4, "data":"04/04/2022", "daCaricare" : true};
-
+matchs['CIS-Q-4'] = {"id":"1360805", "lega":"CIS-Q", "giornata":4, "data":"04/04/2022", "daCaricare" : true};
 //CIS - Finali
 matchs['CIS-F-1'] = {"id":"1405899", "lega":"CIS-F", "giornata":1, "data":"01/08/2022", "daCaricare" : true};
 matchs['CIS-F-2'] = {"id":"1412297", "lega":"CIS-F", "giornata":2, "data":"22/08/2022", "daCaricare" : true};
 matchs['CIS-F-3'] = {"id":"1417645", "lega":"CIS-F", "giornata":3, "data":"12/09/2022", "daCaricare" : true};
 matchs['CIS-F-4'] = {"id":"1422519", "lega":"CIS-F", "giornata":4, "data":"02/10/2022", "daCaricare" : true};
 matchs['CIS-F-5'] = {"id":"1428455", "lega":"CIS-F", "giornata":5, "data":"24/10/2022", "daCaricare" : true};
+matchs['CIS-F-6'] = {"id":"1434345", "lega":"CIS-F", "giornata":5, "data":"14/11/2022", "daCaricare" : true};
+
 
 //EU
 matchs['EU-1'] = {"id":"", "lega":"EU", "giornata":1, "data":"//2022", "daCaricare" : true};
@@ -73,15 +72,15 @@ matchs['EIRC-4-4'] = {"id":"1391733", "lega":"EIRC-4", "giornata":'R1d', "data":
 matchs['EIRC-4-5'] = {"id":"1407335", "lega":"EIRC-4", "giornata":'R2a', "data":"26/07/2022", "daCaricare" : true};
 matchs['EIRC-4-6'] = {"id":"1414935", "lega":"EIRC-4", "giornata":'R2b', "data":"04/09/2022", "daCaricare" : true};
 matchs['EIRC-4-7'] = {"id":"1422969", "lega":"EIRC-4", "giornata":'R2c', "data":"02/10/2022", "daCaricare" : true};
-matchs['EIRC-4-8'] = {"id":"1431409", "lega":"EIRC-4", "giornata":'R2d', "data":"30/10/2022", "daCaricare" : true};
-matchs['EIRC-4-9'] = {"id":"", "lega":"EIRC-4", "giornata":9, "data":"//2022", "daCaricare" : true};
+matchs['EIRC-4-8'] = {"id":"0", "lega":"EIRC-4", "giornata":'R2d', "data":"30/10/2022", "daCaricare" : true,
+"avversarioName":"gewest-zuid-nederland", "risultato":"Persa - Non disputata", "risultatoStyle" :"color:red;font-weight:bold", "giocatori":"", "score":"", "url" : ""};  //Vinto forfait
 
 //1WL - GEO
 matchs['1WL-GEO-1'] = {"id":"0", "lega":"1WL-GEO", "giornata":1, "data":"10/07/2021", "daCaricare" : true,
 "avversarioName":"","risultato":"Bye", "risultatoStyle" :"color:green;font-weight:bold", "giocatori":"", "score":"", "url" : ""}; 
 matchs['1WL-GEO-2'] = {"id":"1410041", "lega":"1WL-GEO", "giornata":2, "data":"14/08/2021", "daCaricare" : true};
 matchs['1WL-GEO-3'] = {"id":"1415951", "lega":"1WL-GEO", "giornata":3, "data":"11/09/2021", "daCaricare" : true};
-Matchs['1WL-GEO-4'] = {"id":"1424191", "lega":"1WL-GEO", "giornata":4, "data":"09/10/2021", "daCaricare" : true};
+matchs['1WL-GEO-4'] = {"id":"1424191", "lega":"1WL-GEO", "giornata":4, "data":"09/10/2021", "daCaricare" : true};
 matchs['1WL-GEO-5'] = {"id":"1429295", "lega":"1WL-GEO", "giornata":5, "data":"30/10/2021", "daCaricare" : true};
 matchs['1WL-GEO-6'] = {"id":"", "lega":"1WL-GEO", "giornata":6, "data":"13/11/2021", "daCaricare" : true};
 matchs['1WL-GEO-7'] = {"id":"", "lega":"1WL-GEO", "giornata":7, "data":"11/12/2021", "daCaricare" : true};
